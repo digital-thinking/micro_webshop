f5a0cb6d519e5205d60051e13d232b026bc0b99e
digital-thinking <github@digital-thinking.de>
2016-12-01 12:05:33 +0100

    move Hystrix dashboard to discovery service

--- server_eureka/src/main/java/de/hska/iwi/vsys/microlab/EurekaApplication.java
+++ server_eureka/src/main/java/de/hska/iwi/vsys/microlab/EurekaApplication.java
@@ -3,9 +3,11 @@ package de.hska.iwi.vsys.microlab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrixDashboard
 public class EurekaApplication {
 
     public static void main(String[] args) {
--- user_proxy/src/main/java/de/hska/iwi/vsys/microlab/UserProxyApplication.java
+++ user_proxy/src/main/java/de/hska/iwi/vsys/microlab/UserProxyApplication.java
@@ -1,16 +1,17 @@
+
 package de.hska.iwi.vsys.microlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
+@EnableHystrix
 @RibbonClient("user-proxy")
 public class UserProxyApplication {
     public static void main(String[] args) {
