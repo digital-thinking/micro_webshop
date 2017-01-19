e418ba2a94a3cbdb9560d609c3da9774f7481829
Sebastian Kolb <kolb.sebastian@gmail.com>
2017-01-19 12:12:25 +0100

    Fehlende Klammer

--- product_proxy/src/main/java/de/hska/iwi/vsys/microlab/api/ProductProxyController.java
+++ product_proxy/src/main/java/de/hska/iwi/vsys/microlab/api/ProductProxyController.java
@@ -95,7 +95,7 @@ public class ProductProxyController {
 		return new ResponseEntity<Object>(saved, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/products/{productId", method = RequestMethod.DELETE)
+	@RequestMapping(value = "/products/{productId}", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> deleteProduct(
 			@ApiParam(value = "The Id of the product to delete", required = true) @PathVariable Long id) {
 		productClient.DeleteProduct(id);
